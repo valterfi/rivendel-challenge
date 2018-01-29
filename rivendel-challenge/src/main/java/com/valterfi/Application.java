b8855935a7622a3659ecfd28e40be3618e680931
Valterfi de Oliveira Costa <valterfi.oliveira@gmail.com>
2018-01-29 02:57:56 -0300

    iniciando algumas configuracoes para comecar implementacao de testes unitarios

--- rivendel-challenge/src/main/java/com/valterfi/Application.java
+++ rivendel-challenge/src/main/java/com/valterfi/Application.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableConfigurationProperties
 @SpringBootApplication
 @ComponentScan("com.valterfi")
-@EnableSwagger2
 public class Application {
     
     public static void main(String[] args) throws Exception {
