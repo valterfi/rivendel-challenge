46d7dd77161521e6d64128906dd7448fa44943fd
Valterfi de Oliveira Costa <valterfi.oliveira@gmail.com>
2018-01-31 03:10:09 -0300

    retirando espaço em branco

--- rivendel-challenge/src/main/java/com/valterfi/elasticsearch/domain/EsActivity.java
+++ rivendel-challenge/src/main/java/com/valterfi/elasticsearch/domain/EsActivity.java
@@ -6,7 +6,7 @@ import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
-import com.fasterxml.jackson.annotation.  JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
