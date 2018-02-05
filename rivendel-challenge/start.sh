28463638118301bde48f3d0c8f9588032d20c5c1
Valterfi de Oliveira Costa <valterfi.oliveira@gmail.com>
2018-02-05 02:29:08 -0300

    mudanças para dockerizar aplicação

---
+++ rivendel-challenge/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+gosu valterfi elasticsearch -d --cluster.name valterfi-cluster 
+nohup logstash --allow-env -f ${RIVENDEL_APP_HOME}/src/main/resources/logstash/conf &
+exec gosu valterfi java -jar /opt/rivendel-challenge/target/rivendel-challenge-1.0.1-SNAPSHOT.jar
