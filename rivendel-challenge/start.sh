a995b7faf45e0612835b6c643b0b7c0f9650a98d
Valterfi de Oliveira Costa <valterfi.oliveira@gmail.com>
2018-02-05 02:41:23 -0300

    incrementando versao para 1.1.0

--- rivendel-challenge/start.sh
+++ rivendel-challenge/start.sh
@@ -5,4 +5,4 @@ set -e
 
 gosu valterfi elasticsearch -d --cluster.name valterfi-cluster 
 nohup logstash --allow-env -f ${RIVENDEL_APP_HOME}/src/main/resources/logstash/conf &
-exec gosu valterfi java -jar /opt/rivendel-challenge/target/rivendel-challenge-1.0.1-SNAPSHOT.jar
+exec gosu valterfi java -jar /opt/rivendel-challenge/target/rivendel-challenge-1.1.0.jar
